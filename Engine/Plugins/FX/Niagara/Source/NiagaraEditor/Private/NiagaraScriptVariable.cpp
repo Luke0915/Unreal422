8b479b9161b72b8c38a7ed895be638dba0c4e274
Morten Vassvik <Morten.Vassvik@xa.epicgames.com>
2019-05-13 03:25:49 -0400

    Add missing copyright notices
    #rb none
    
    [CL 6425845 by Morten Vassvik in Dev-Niagara branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraScriptVariable.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraScriptVariable.cpp
@@ -1,3 +1,5 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
 #include "NiagaraScriptVariable.h"
 
 UNiagaraScriptVariable::UNiagaraScriptVariable(const FObjectInitializer& ObjectInitializer)
