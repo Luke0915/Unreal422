1b74cfb46b2667241f4a4f2621c1cdadbf5efa99
Shaun Kime <Shaun.Kime@epicgames.com>
2019-05-29 13:59:39 -0400

    Merging //UE4/Dev-Main to Dev-Niagara (//UE4/Dev-Niagara) Just catching up to main for latest #tests enginetests for Niagara pass #rb none
    [CL 6665515 by Shaun Kime in Dev-Niagara branch]

--- Engine/Source/Runtime/Renderer/Private/MobileShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/MobileShadingRenderer.cpp
@@ -572,7 +572,7 @@ void FMobileSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 	{
 	    RHICmdList.SetCurrentStat(GET_STATID(STAT_CLMM_Occlusion));
 		// flush
-		RHICmdList.SubmitCommandsAndFlushGPU();
+		RHICmdList.SubmitCommandsHint();
 		// Issue occlusion queries
 	    RenderOcclusion(RHICmdList);
 	    RHICmdList.ImmediateFlush(EImmediateFlushType::DispatchToRHIThread);
@@ -586,6 +586,12 @@ void FMobileSceneRenderer::Render(FRHICommandListImmediate& RHICmdList)
 	
 	// End of scene color rendering
 	RHICmdList.EndRenderPass();
+
+	if (!bGammaSpace || bRenderToSceneColor)
+	{
+		// transition scene color to Readable for post-processing
+		RHICmdList.TransitionResource(EResourceTransitionAccess::EReadable, SceneColor);
+	}
 
 	RHICmdList.SetCurrentStat(GET_STATID(STAT_CLMM_Post));
 
